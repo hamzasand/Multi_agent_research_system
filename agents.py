9d48b96db74a3a11d52fc41dae900e507e59b1e9
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-09-25 16:38:52 +0500

    Add import for ChatPromptTemplate

--- agents.py
+++ agents.py
@@ -1,6 +1,7 @@
 from langchain.agents import create_agent
 
 from langchain_openai import ChatOpenAI
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
