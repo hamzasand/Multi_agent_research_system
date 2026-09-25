ad92b50ec4d5bcc83950ce68a5ccc0e1f98555ce
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-09-25 16:39:07 +0500

    Add StrOutputParser import to agents.py

--- agents.py
+++ agents.py
@@ -3,6 +3,7 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 
 from langchain_core.prompts import ChatPromptTemplate
+
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
 from dotenv import load_dotenv
